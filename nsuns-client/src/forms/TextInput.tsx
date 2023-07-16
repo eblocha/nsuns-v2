d9c7a07ad0fa8bfc1f87abb3b80645bfeb545f9d
eblocha <eblocha@protonmail.com>
2023-07-16 18:08:49 -0400

    improve error handling for create-user form

--- nsuns-client/src/forms/TextInput.tsx
+++ nsuns-client/src/forms/TextInput.tsx
@@ -1,4 +1,4 @@
-import { Show, type Component } from "solid-js";
+import { Show, type Component, createRenderEffect } from "solid-js";
 import { IFormControl } from "solid-forms";
 import style from "./TextInput.module.css";
 
@@ -8,26 +8,40 @@ export const TextInput: Component<{
   type?: string;
   class?: string;
 }> = (props) => {
+
+  createRenderEffect(() => {
+    if (!props.control.isRequired || props.control.value.length > 0) {
+      props.control.setErrors(null);
+    } else {
+      props.control.setErrors({ isMissing: true });
+    }
+  });
+
+  const showErrors = () => {
+    return !!props.control.errors && props.control.isDirty;
+  }
+
   return (
-    <>
+    <div class="flex flex-col items-end">
       <input
         class={props.class}
         classList={{
-          [style.invalid]: !!props.control.errors,
+          [style.invalid]: showErrors(),
         }}
         name={props.name}
         type={props.type}
         value={props.control.value}
         oninput={(e) => {
           props.control.setValue(e.currentTarget.value);
+          props.control.markDirty(true);
         }}
         onblur={() => props.control.markTouched(true)}
+        disabled={props.control.isDisabled}
         required={props.control.isRequired}
       />
-
-      <Show when={props.control.isTouched && props.control.errors?.isMissing}>
-        <small>Answer required.</small>
+      <Show when={showErrors() && props.control.errors?.isMissing}>
+        <small class="text-red-500">This field is required.</small>
       </Show>
-    </>
+    </div>
   );
 };
--- nsuns-client/src/login/CreateUser.tsx
+++ nsuns-client/src/login/CreateUser.tsx
@@ -1,11 +1,13 @@
 import { createFormControl, createFormGroup } from "solid-forms";
-import { Component } from "solid-js";
+import { Component, Show, createSignal } from "solid-js";
 import { TextInput } from "../forms/TextInput";
 import { A, useNavigate } from "@solidjs/router";
 import { createUser } from "../api";
 import styles from "./CreateUser.module.css";
 
 export const CreateUser: Component = () => {
+  const [submitError, setSubmitError] = createSignal<unknown>(null);
+
   const navigator = useNavigate();
 
   const group = createFormGroup({
@@ -18,13 +20,28 @@ export const CreateUser: Component = () => {
   const onSubmit = async () => {
     if (group.isSubmitted || !group.isValid) return;
 
+    setSubmitError(null);
     group.markSubmitted(true);
-    const user = await createUser({
-      username: group.value.username ?? "user",
-      name: group.value.name || null,
-    });
-    navigator(`/users/${user.id}`);
+    try {
+      const user = await createUser({
+        username: group.value.username ?? "user",
+        name: group.value.name || null,
+      });
+      navigator(`/users/${user.id}`);
+    } catch (e) {
+      setSubmitError(e);
+    } finally {
+      group.markSubmitted(false);
+    }
   };
+
+  const resetForm = () => {
+    for (const control of Object.values(group.controls)) {
+      control.markDirty(false);
+      control.markTouched(false);
+      control.setValue("");
+    }
+  }
 
   return (
     <div class="w-full h-full flex flex-col items-center justify-center">
@@ -54,14 +71,31 @@ export const CreateUser: Component = () => {
           name="name"
         />
         <div class="col-span-2">
-          <div class="float-right flex flex-row items-center">
-            <A href="/" class="bg-gray-200 p-2 rounded hover:bg-gray-300 text-center mr-2">
+          <div class="float-right flex flex-row items-center justify-end w-full">
+            <A
+              href="/"
+              class="bg-gray-300 p-2 rounded hover:bg-gray-400 text-center mr-2"
+            >
               Home
             </A>
-            <button class="bg-blue-500 text-white p-2 rounded hover:bg-blue-600">
-              Create User
+            <button
+              type="button"
+              onClick={resetForm}
+              class="bg-gray-300 p-2 rounded hover:bg-gray-400 text-center mr-2 disabled:text-gray-600 disabled:bg-gray-200"
+              disabled={!group.isDirty || group.isSubmitted}
+            >
+              Reset
+            </button>
+            <button
+              class="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 disabled:bg-blue-300"
+              disabled={!group.isValid || group.isSubmitted}
+            >
+              <Show when={!group.isSubmitted} fallback={<>Creating...</>}>
+                Create User
+              </Show>
             </button>
           </div>
+          <Show when={submitError() !== null}>{`${submitError()}`}</Show>
         </div>
       </form>
     </div>
