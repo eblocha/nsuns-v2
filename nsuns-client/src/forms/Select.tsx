b5f5e51316adef942e9bbe2d663223dfc3bee9c2
eblocha <eblocha@protonmail.com>
2023-07-21 10:12:02 -0400

    Add Select component, use in NewSet form

---
+++ nsuns-client/src/forms/Select.tsx
@@ -0,0 +1,79 @@
+import { Component, For, JSX, createMemo, createRenderEffect } from "solid-js";
+import { Control } from "../hooks/forms";
+import style from "./Input.module.css";
+
+export type SelectOption = {
+  value: string;
+  name?: string;
+};
+
+export const EMPTY_OPTION: SelectOption = {
+  value: "",
+  name: "",
+};
+
+export const Select: Component<
+  JSX.SelectHTMLAttributes<HTMLSelectElement> & {
+    control: Control<string>;
+    options?: SelectOption[];
+    emptyOption?: string;
+    onBlur?: JSX.FocusEventHandler<HTMLSelectElement, FocusEvent>;
+    onInput?: JSX.InputEventHandler<HTMLSelectElement, InputEvent>;
+  }
+> = (props) => {
+  const options = createMemo(() => {
+    if (!props.options?.length) {
+      return [
+        {
+          value: "",
+          name: props.emptyOption,
+        },
+      ];
+    }
+
+    if (props.emptyOption === undefined) {
+      return props.options;
+    }
+
+    return [
+      {
+        value: "",
+        name: props.emptyOption,
+      },
+      ...props.options,
+    ];
+  });
+
+  createRenderEffect(() => {
+    const firstOption = options()[0].value;
+    if (!props.control.value() && firstOption) {
+      props.control.setValue(firstOption);
+    }
+  });
+
+  return (
+    <select
+      {...props}
+      classList={{
+        [style.invalid]: props.control.showErrors(),
+        ...props.classList,
+      }}
+      value={props.control.value()}
+      onInput={(e) => {
+        props.control.setValue(e.target.value);
+        props.control.setDirty(true);
+        props.onInput?.(e);
+      }}
+      onBlur={(e) => {
+        props.control.setTouched(true);
+        props.onBlur?.(e);
+      }}
+    >
+      <For each={options()}>
+        {(option) => (
+          <option value={option.value}>{option.name ?? option.value}</option>
+        )}
+      </For>
+    </select>
+  );
+};
--- nsuns-client/src/program/builder/NewSet.tsx
+++ nsuns-client/src/program/builder/NewSet.tsx
@@ -1,8 +1,11 @@
-import { Component, For, Show } from "solid-js";
+import { Component, Show, createEffect, createMemo } from "solid-js";
 import { Day, Movement } from "../../api";
 import { useCreateSet } from "../../hooks/queries/sets";
 import { Spinner } from "../../icons/Spinner";
 import { createControl, createControlGroup, required } from "../../hooks/forms";
+import { Select, SelectOption } from "../../forms/Select";
+import { Input } from "../../forms/Input";
+import { TextArea } from "../../forms/TextArea";
 
 export const NewSet: Component<{
   close: () => void;
@@ -19,7 +22,7 @@ export const NewSet: Component<{
   const group = createControlGroup({
     movementId: createControl<string>("", { validators: [required()] }),
     reps: createControl<string>(""),
-    repsIsMinimum: createControl<boolean>(true),
+    repsIsMinimum: createControl("true"),
     description: createControl<string>(""),
     amount: createControl<string>("0", { validators: [required()] }),
     maxMovementId: createControl<string>(""),
@@ -28,7 +31,7 @@ export const NewSet: Component<{
   const onSubmit = () => {
     if (mutation.isLoading || group.hasErrors()) return;
     const value = group.value();
-    
+
     if (!value.amount || !value.movementId) return;
 
     mutation.mutate({
@@ -36,16 +39,25 @@ export const NewSet: Component<{
       day: props.dayIndex as Day,
       description: value.description ?? null,
       movementId: parseInt(value.movementId),
-      percentageOfMax: value.maxMovementId ? parseInt(value.maxMovementId) : null,
+      percentageOfMax: value.maxMovementId
+        ? parseInt(value.maxMovementId)
+        : null,
       programId: props.programId,
       reps: value.reps ? parseInt(value.reps) : null,
-      repsIsMinimum: value.repsIsMinimum ?? false,
+      repsIsMinimum: value.repsIsMinimum === "true",
     });
   };
 
+  const movementOptions = createMemo<SelectOption[] | undefined>(() =>
+    props.movements?.map((movement) => ({
+      value: movement.id.toString(),
+      name: movement.name,
+    }))
+  );
+
   return (
     <form
-      class="grid grid-cols-3 border border-gray-700 p-4 rounded"
+      class="grid grid-cols-2 gap-y-4 gap-x-2 border border-gray-700 p-4 rounded"
       onSubmit={(e) => {
         e.preventDefault();
         onSubmit();
@@ -54,35 +66,69 @@ export const NewSet: Component<{
       <h3 class="col-span-3 text-lg mb-2">Add Set</h3>
       <label>
         <span class="text-red-500">*</span>Movement:
-        <select
-          class="input ml-3"
-          value={group.controls.movementId.value() ?? undefined}
-          onChange={(e) => {
-            group.controls.movementId.setValue(e.target.value);
-            group.controls.movementId.setDirty(true);
-          }}
-          onBlur={() => group.controls.movementId.setTouched(true)}
-        >
-          <For each={props.movements}>
-            {(movement) => <option value={movement.id}>{movement.name}</option>}
-          </For>
-        </select>
-      </label>
-      <label>
-        Reps:
-        <input
-          class="input ml-3"
-          type="number"
-          min={0}
-          value={group.controls.reps.value() ?? undefined}
+        <Select
+          class="input w-full"
+          control={group.controls.movementId}
+          options={movementOptions()}
+          required={true}
         />
       </label>
-      <label class="flex flex-row items-center">
-        <input class="mr-3" type="checkbox" />
-        Is Minimum?
+      <div>
+        <label class="w-full" for="reps">
+          Reps:
+        </label>
+        <div class="grid grid-cols-2 gap-2">
+          <Input
+            control={group.controls.reps}
+            class="input w-full"
+            type="number"
+            name="reps"
+            min={0}
+          />
+          <label class="flex flex-row items-center">
+            <Input
+              control={group.controls.repsIsMinimum}
+              class="mr-3"
+              type="checkbox"
+            />
+            Is Minimum?
+          </label>
+        </div>
+      </div>
+
+      <div class="col-span-2 grid grid-cols-2 gap-2">
+        <label>
+          <span class="text-red-500">*</span>
+          <Show
+            when={!group.controls.maxMovementId.value()}
+            fallback={"Percent:"}
+          >
+            Weight:
+          </Show>
+          <Input
+            control={group.controls.amount}
+            class="input w-full"
+            type="number"
+            min={0}
+            required={true}
+          />
+        </label>
+        <label>
+          Percent of Max In:
+          <Select
+            control={group.controls.maxMovementId}
+            class="input w-full"
+            options={movementOptions()}
+            emptyOption=""
+          />
+        </label>
+      </div>
+      <label class="col-span-2">
+        Description:
+        <TextArea control={group.controls.description} class="input w-full" />
       </label>
 
-      <div class="col-span-3 flex flex-row items-center justify-end gap-2 mt-3">
+      <div class="col-span-2 flex flex-row items-center justify-end gap-2">
         <button type="button" class="secondary-button" onClick={props.close}>
           Cancel
         </button>
