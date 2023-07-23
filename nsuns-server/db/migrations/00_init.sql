f84b9bf18558b4a4b38b142d3b37c37b41e0d4c6
eblocha <eblocha@protonmail.com>
2023-07-23 13:48:10 -0400

    Format migrations sql

--- nsuns-server/db/migrations/00_init.sql
+++ nsuns-server/db/migrations/00_init.sql
@@ -2,6 +2,7 @@ CREATE TABLE profiles (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   name VARCHAR NOT NULL
 );
+
 CREATE TABLE programs (
   id SERIAL PRIMARY KEY,
   name VARCHAR,
@@ -10,6 +11,7 @@ CREATE TABLE programs (
   -- profile that owns the program
   owner UUID NOT NULL REFERENCES profiles(id) ON DELETE CASCADE
 );
+
 -- bench press, squat, etc.
 CREATE TABLE movements (
   id SERIAL PRIMARY KEY,
@@ -17,6 +19,7 @@ CREATE TABLE movements (
   description VARCHAR,
   CONSTRAINT unique_movement_name UNIQUE (name)
 );
+
 CREATE TABLE program_sets (
   id SERIAL PRIMARY KEY,
   program_id INTEGER NOT NULL REFERENCES programs(id) ON DELETE CASCADE,
@@ -38,7 +41,9 @@ CREATE TABLE program_sets (
   percentage_of_max INTEGER REFERENCES movements(id) ON DELETE SET NULL,
   CONSTRAINT unique_set UNIQUE (program_id, day, movement_id, ordering)
 );
+
 CREATE INDEX sets_by_program_id ON program_sets(program_id);
+
 CREATE TABLE maxes (
   id SERIAL PRIMARY KEY,
   profile_id UUID NOT NULL REFERENCES profiles(id) ON DELETE CASCADE,
@@ -46,12 +51,18 @@ CREATE TABLE maxes (
   timestamp TIMESTAMP DEFAULT now(),
   amount DOUBLE PRECISION NOT NULL CHECK (amount >= 0)
 );
+
 CREATE INDEX maxes_by_profile_id ON maxes(profile_id);
+
 CREATE TABLE reps (
   id SERIAL PRIMARY KEY,
   profile_id UUID NOT NULL REFERENCES profiles(id) ON DELETE CASCADE,
   movement_id INTEGER NOT NULL REFERENCES movements(id) ON DELETE CASCADE,
   timestamp TIMESTAMP DEFAULT now(),
-  amount INTEGER CHECK (amount IS NULL OR amount >= 0)
+  amount INTEGER CHECK (
+    amount IS NULL
+    OR amount >= 0
+  )
 );
+
 CREATE INDEX reps_by_profile_id ON reps(profile_id);
