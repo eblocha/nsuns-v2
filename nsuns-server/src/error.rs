998b8da79d1b809a5f808a5d38d124bdb2c8a49d
eblocha <eblocha@protonmail.com>
2023-07-24 17:06:40 -0400

    Use fmt::Display of the error value in Display impl for Error

--- nsuns-server/src/error.rs
+++ nsuns-server/src/error.rs
@@ -14,7 +14,7 @@ pub type Result<T> = core::result::Result<T, Error>;
 
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}: {:?}", self.status, self.error)
+        Display::fmt(&self.error, f)
     }
 }
 
