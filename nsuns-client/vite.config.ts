ef149bf70e25eb6475f1bae4c1c428f27720e01a
eblocha <eblocha@protonmail.com>
2023-08-12 11:03:50 -0400

    Add cargo-make Makefile, move client gzipping step to vite build

--- nsuns-client/vite.config.ts
+++ nsuns-client/vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
-// import devtools from 'solid-devtools/vite';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
   plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
     solidPlugin(),
+    viteCompression({
+      filter: /\.(js|css|ico)$/i
+    })
   ],
   server: {
     proxy: {
@@ -19,8 +17,5 @@ export default defineConfig({
   },
   build: {
     target: "esnext",
-    outDir: "../dist/assets",
-    emptyOutDir: true,
-    assetsDir: "."
   },
 });
