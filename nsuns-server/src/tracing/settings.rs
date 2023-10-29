0693cef6c9182dc7599280a2ca1961ed7bdfb0c6
eblocha <eblocha@protonmail.com>
2023-10-29 08:53:28 -0400

    Switch to otlp

--- nsuns-server/src/tracing/settings.rs
+++ nsuns-server/src/tracing/settings.rs
@@ -1,14 +1,40 @@
+use std::time::Duration;
+
 use serde::Deserialize;
 
 use crate::feature::Feature;
 
+pub fn default_exporter_host() -> String {
+    // jaeger default port
+    "http://localhost:4317".to_string()
+}
+
+pub fn default_exporter_timeout() -> Duration {
+    Duration::from_secs(3)
+}
+
 #[derive(Debug, Deserialize)]
-pub struct OpenTelemetrySettings {}
+#[serde(default)]
+pub struct OpenTelemetrySettings {
+    #[serde(default = "default_exporter_host")]
+    pub exporter_host: String,
+    #[serde(default = "default_exporter_timeout")]
+    pub exporter_timeout: Duration,
+}
+
+impl Default for OpenTelemetrySettings {
+    fn default() -> Self {
+        Self {
+            exporter_host: default_exporter_host(),
+            exporter_timeout: default_exporter_timeout(),
+        }
+    }
+}
 
 pub type OpenTelemetryFeature = Feature<OpenTelemetrySettings>;
 
 impl Default for OpenTelemetryFeature {
     fn default() -> Self {
-        Feature::Enabled(OpenTelemetrySettings {})
+        Self::Disabled
     }
 }
--- nsuns-server/src/tracing/setup.rs
+++ nsuns-server/src/tracing/setup.rs
@@ -1,4 +1,12 @@
 use anyhow::Context;
+use opentelemetry::KeyValue;
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::{
+    runtime::Tokio,
+    trace::{self, RandomIdGenerator, Sampler},
+    Resource,
+};
+use opentelemetry_semantic_conventions as semcov;
 use tracing_subscriber::prelude::*;
 
 use super::settings::OpenTelemetryFeature;
@@ -10,10 +18,27 @@ pub fn setup_tracing(settings: &OpenTelemetryFeature) -> anyhow::Result<()> {
         )
         .with(tracing_subscriber::fmt::layer());
 
-    if settings.is_enabled() {
-        let tracer = opentelemetry_jaeger::new_agent_pipeline()
-            .with_service_name("nsuns")
-            .install_simple()
+    if let OpenTelemetryFeature::Enabled(settings) = settings {
+        let otlp_exporter = opentelemetry_otlp::new_exporter()
+            .tonic()
+            .with_endpoint(&settings.exporter_host)
+            .with_timeout(settings.exporter_timeout);
+
+        let tracer = opentelemetry_otlp::new_pipeline()
+            .tracing()
+            .with_exporter(otlp_exporter)
+            .with_trace_config(
+                trace::config()
+                    .with_sampler(Sampler::AlwaysOn)
+                    .with_id_generator(RandomIdGenerator::default())
+                    .with_max_events_per_span(64)
+                    .with_max_attributes_per_span(16)
+                    .with_resource(Resource::new(vec![KeyValue::new(
+                        semcov::resource::SERVICE_NAME,
+                        "nsuns",
+                    )])),
+            )
+            .install_batch(Tokio)
             .with_context(|| "failed to install otel tracer")?;
 
         let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
