f46ee00b32b6a08850df74d5f7b3939da8fd1f9a
eblocha <eblocha@protonmail.com>
2024-04-21 14:42:47 -0400

    Create a more realistic load test scenario

--- k6/load-test.js
+++ k6/load-test.js
@@ -3,22 +3,25 @@ import { check, sleep } from "k6";
 
 const host = __ENV.TARGET_HOST || "host.docker.internal:8080";
 
+const min = 1000;
+const max = 5000;
+
 export const options = {
   thresholds: {
     http_req_duration: ["p(99) < 3000"],
   },
   // Ramp the number of virtual users up and down
   stages: [
-    { duration: "30s", target: 1000 },
-    { duration: "1m", target: 1000 },
-    { duration: "30s", target: 5000 },
-    { duration: "1m", target: 5000 },
-    { duration: "30s", target: 1000 },
-    { duration: "1m", target: 1000 },
-    { duration: "30s", target: 5000 },
-    { duration: "1m", target: 5000 },
-    { duration: "30s", target: 1000 },
-    { duration: "1m", target: 1000 },
+    { duration: "30s", target: min },
+    { duration: "1m", target: min },
+    { duration: "30s", target: max },
+    { duration: "1m", target: max },
+    { duration: "30s", target: min },
+    { duration: "1m", target: min },
+    { duration: "30s", target: max },
+    { duration: "1m", target: max },
+    { duration: "30s", target: min },
+    { duration: "1m", target: min },
   ],
 };
 
@@ -31,22 +34,24 @@ let program;
 
 const checks = {
   "is created": (response) => response.status === 201,
-  "body is json": (response) => response.body.startsWith("{")
+  "body is json": (response) => response.body.startsWith("{"),
 };
 
 export default function () {
   if (!profile) {
+    const sets = 8;
+
+    const headers = {
+      "content-type": "application/json",
+    };
+
     // create a profile
     const profiles_res = http.post(
       `http://${host}/api/profiles`,
       JSON.stringify({
         name: randomName(),
       }),
-      {
-        headers: {
-          "content-type": "application/json",
-        },
-      }
+      { headers }
     );
 
     check(profiles_res, checks);
@@ -54,6 +59,7 @@ export default function () {
     profile = JSON.parse(profiles_res.body);
 
     sleep(0.5);
+
     // create a program
     const program_res = http.post(
       `http://${host}/api/programs`,
@@ -61,18 +67,52 @@ export default function () {
         name: randomName(),
         owner: profile.id,
       }),
-      {
-        headers: {
-          "content-type": "application/json",
-        },
-      }
+      { headers }
     );
 
     check(program_res, checks);
 
     program = JSON.parse(program_res.body);
 
-    sleep(0.1);
+    // add workouts to program
+    for (let day = 1; day < 6; day++) {
+      sleep(0.5);
+
+      // create a movement for the day
+      const res = http.post(
+        `http://${host}/api/movements`,
+        JSON.stringify({
+          name: randomName(),
+        }),
+        { headers }
+      );
+
+      check(res, checks);
+
+      const movement_id = JSON.parse(res.body).id;
+
+      for (let i = 0; i < sets; i++) {
+        sleep(0.5);
+
+        // create a set for the movement for the day
+        const set_res = http.post(
+          `http://${host}/api/sets`,
+          JSON.stringify({
+            program_id: program.id,
+            movement_id,
+            day,
+            reps: 8,
+            amount: 10,
+            percentage_of_max: movement_id,
+          }),
+          { headers }
+        );
+
+        check(set_res, checks);
+      }
+    }
+
+    sleep(1);
   }
 
   // get the summary
@@ -80,8 +120,8 @@ export default function () {
 
   check(res, {
     "is ok": (response) => response.status === 200,
-    "body is json": (response) => response.body.startsWith("{")
+    "body is json": (response) => response.body.startsWith("{"),
   });
 
-  sleep(0.1);
+  sleep(1);
 }
