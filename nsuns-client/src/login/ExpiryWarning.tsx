eb98e3179834042810c53e425f917a82e11ca1ec
eblocha <eblocha@protonmail.com>
2024-04-27 08:45:44 -0400

    Add option to dismiss expiry warning

--- nsuns-client/src/login/ExpiryWarning.tsx
+++ nsuns-client/src/login/ExpiryWarning.tsx
@@ -1,7 +1,10 @@
-import { Component, Show } from "solid-js";
+import { Component, Show, createEffect, createSignal } from "solid-js";
 import { useUserInfoQuery } from "../hooks/queries/auth";
+import { ChevronLeft } from "../icons/ChevronLeft";
 
 export const ExpiryWarning: Component = () => {
+  const [isVisible, setVisible] = createSignal(false);
+
   const userInfo = useUserInfoQuery();
 
   const expiry = () => {
@@ -13,11 +16,37 @@ export const ExpiryWarning: Component = () => {
     return dt.toDateString() + " at " + dt.toLocaleTimeString(undefined, { hour: "2-digit", minute: "2-digit" });
   };
 
+  const isAnonymous = () => userInfo.isSuccess && userInfo.data.type === "anonymous";
+
+  createEffect(() => {
+    if (isAnonymous()) {
+      setVisible(true);
+    }
+  });
+
   return (
-    <Show when={userInfo.isSuccess && userInfo.data.type === "anonymous"}>
-      <div class="absolute bottom-0 right-0 m-5 rounded border border-gray-500 p-3 bg-gray-900">
-        Your free trial period will end on {expiry()}.
-      </div>
+    <Show when={isAnonymous()}>
+      <Show
+        when={isVisible()}
+        fallback={
+          <button
+            class="absolute bottom-0 right-0 m-5 secondary-button"
+            onClick={() => setVisible(true)}
+          >
+            <ChevronLeft />
+          </button>
+        }
+      >
+        <div class="absolute bottom-0 right-0 m-5 rounded border border-gray-500 p-3 bg-gray-900 flex flex-row items-center gap-2">
+          <button
+            class="text-button"
+            onClick={() => setVisible(false)}
+          >
+            <ChevronLeft class="rotate-180" />
+          </button>
+          Your free trial period will end on {expiry()}.
+        </div>
+      </Show>
     </Show>
   );
 };
