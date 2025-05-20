797f9cf8663498891a305fc2ca3b94eb16b6532d
eblocha <eblocha@protonmail.com>
2025-05-20 07:54:02 -0400

    Add a fullscreen button to program runner

--- nsuns-client/src/program/runner/simple/SimpleProgramRunner.tsx
+++ nsuns-client/src/program/runner/simple/SimpleProgramRunner.tsx
@@ -1,11 +1,25 @@
 import { A, useParams } from "@solidjs/router";
-import { Component } from "solid-js";
+import { Component, createSignal, Show } from "solid-js";
 import { ProgramProvider } from "../context/ProgramProvider";
 import { User } from "../../../icons/User";
 import { Edit } from "../../../icons/Edit";
 import { TitleBanner } from "./SetTitle";
 import { Tools } from "./Tools";
 import { DataList } from "./data/DataList";
+import { Maximize } from "../../../icons/Maximize";
+import { Minimize } from "../../../icons/Minimize";
+
+const [isFullscreen, setIsFullscreen] = createSignal(!!document.fullscreenElement);
+
+function toggleFullscreen() {
+  if (!document.fullscreenElement) {
+    setIsFullscreen(true);
+    void document.documentElement.requestFullscreen();
+  } else {
+    setIsFullscreen(false);
+    void document.exitFullscreen();
+  }
+}
 
 export const SimpleProgramRunner: Component = () => {
   const params = useParams<{ programId: string; profileId: string }>();
@@ -30,6 +44,17 @@ export const SimpleProgramRunner: Component = () => {
             >
               <Edit />
             </A>
+            <button
+              class="text-button text-2xl"
+              onClick={toggleFullscreen}
+            >
+              <Show
+                when={isFullscreen()}
+                fallback={<Maximize />}
+              >
+                <Minimize />
+              </Show>
+            </button>
           </nav>
           <div class="p-4 flex-grow flex flex-col gap-4">
             <TitleBanner />
